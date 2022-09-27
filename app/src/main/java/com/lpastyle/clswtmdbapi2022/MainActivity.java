442c5d339c9281fee29e987780aed74715605cfb
lpastyle <lpastorelli@lpastyle.com>
2022-09-27 15:05:42 +0200

    Final version based on view bindings

--- app/src/main/java/com/lpastyle/clswtmdbapi2022/MainActivity.java
+++ app/src/main/java/com/lpastyle/clswtmdbapi2022/MainActivity.java
@@ -1,17 +1,17 @@
 package com.lpastyle.clswtmdbapi2022;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import com.lpastyle.clswtmdbapi2022.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +20,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -30,72 +30,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     PersonListAdapter mPersonListAdapter = null;
 
     // views
-    Context mContext = this;
-    Button mNextBt;
-    Button mPrevBt;
-    RecyclerView mPopularPersonRv;
-    TextView mPageNumberTv;
+    private final Context mContext = this;
+    // private RecyclerView mPopularPersonRv;
+    // private TextView mPageNumberTv;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // init API client
-        if (ApiClient.get() != null) tmdbApi = ApiClient.get().create(ITmdbApi.class);
-
-        // set button listeners
-        mNextBt = findViewById(R.id.next_page_bt);
-        mPrevBt = findViewById(R.id.prev_page_bt);
-        mNextBt.setOnClickListener(this);
-        mPrevBt.setOnClickListener(this);
+        if (ApiClient.get() != null)
+            tmdbApi = ApiClient.get().create(ITmdbApi.class);
+        else
+            finish(); // ends activity
 
         // Current page number text view
-        mPageNumberTv = findViewById(R.id.page_number_tv);
+        //mPageNumberTv = findViewById(R.id.page_number_tv);
         setPageNumber();
 
-
         // set recycler view
-        mPopularPersonRv = findViewById(R.id.popular_person_rv);
-        mPopularPersonRv.setHasFixedSize(true);
+        //mPopularPersonRv = findViewById(R.id.popular_person_rv);
+        //mPopularPersonRv.setHasFixedSize(true);
+        binding.popularPersonRv.setHasFixedSize(true);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this); // use a linear layout manager
-        mPopularPersonRv.setLayoutManager(layoutManager);
+        //mPopularPersonRv.setLayoutManager(layoutManager);
+        binding.popularPersonRv.setLayoutManager(layoutManager);
 
         mPersonListAdapter = new PersonListAdapter(results); // attach a custom adapter
-        mPopularPersonRv.setAdapter(mPersonListAdapter);
+        //mPopularPersonRv.setAdapter(mPersonListAdapter);
+        binding.popularPersonRv.setAdapter(mPersonListAdapter);
 
         // call TMDB api
-        refreshPersons();
-
+        refreshPopularPersons();
     }
 
-    private void refreshPersons() {
+    private void refreshPopularPersons() {
         if (tmdbApi != null) {
             Call<PersonPopularResponse> call = tmdbApi.getPersonPopular(ITmdbApi.KEY, String.valueOf(mCurrentPage));
             call.enqueue(new Callback<PersonPopularResponse>() {
                 @Override
                 public void onResponse(@NonNull Call<PersonPopularResponse> call, @NonNull Response<PersonPopularResponse> response) {
                     results.clear();
-                    switch (response.code()) {
-                        case 200:
-                            PersonPopularResponse personResponse = response.body();
-                            if (personResponse != null && personResponse.getResults() != null) {
-                                results.addAll(personResponse.getResults());
-                                Log.d(LOG_TAG, "Number of popular person found=" + results.size());
-                            }
-                            break;
-                        default:
-                            Log.e(LOG_TAG, "HTTP error " + response.code());
-                            results.clear();
-                            mCurrentPage = 0;
-                            Toast toast = Toast.makeText(mContext, R.string.toast_http_error, Toast.LENGTH_LONG);
-                            toast.show();
-                            break;
-
+                    if (response.code() == 200) {
+                        PersonPopularResponse personResponse = response.body();
+                        if (personResponse != null && personResponse.getResults() != null) {
+                            results.addAll(personResponse.getResults());
+                            Log.d(LOG_TAG, "Number of popular person found=" + results.size());
+                        }
+                    } else {
+                        Log.e(LOG_TAG, "HTTP error " + response.code());
+                        results.clear();
+                        mCurrentPage = 0;
+                        Toast toast = Toast.makeText(mContext, R.string.toast_http_error, Toast.LENGTH_LONG);
+                        toast.show();
                     }
-                    mPersonListAdapter.notifyDataSetChanged();
-                    mPopularPersonRv.scrollToPosition(0);
+                    //mPersonListAdapter.notifyDataSetChanged();
+                    //mPopularPersonRv.scrollToPosition(0);
+                    mPersonListAdapter.notifyItemRangeChanged(0,results.size());
+                    binding.popularPersonRv.scrollToPosition(0);
                     setPageNumber();
                 }
 
@@ -104,7 +101,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Log.e(LOG_TAG, "Call to 'getPersonPopular' failed");
                     results.clear();
                     mCurrentPage = 0;
-                    mPersonListAdapter.notifyDataSetChanged();
+                    //mPersonListAdapter.notifyDataSetChanged();
+                    mPersonListAdapter.notifyItemRangeChanged(0,0);
                     setPageNumber();
                     Toast toast = Toast.makeText(mContext, R.string.toast_network_error, Toast.LENGTH_LONG);
                     toast.show();
@@ -120,26 +118,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     // helper to update page number caption
     private void setPageNumber() {
         String caption = getResources().getString(R.string.page_number, mCurrentPage);
-        mPageNumberTv.setText(caption);
+        //mPageNumberTv.setText(caption);
+        binding.pageNumberTv.setText(caption);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.prev_page_bt:
-                if (mCurrentPage > 1) {
-                    mCurrentPage--;
-                    refreshPersons();
-                } else {
-                    Toast toast = Toast.makeText(this, R.string.toast_no_previous_page, Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-                break;
-            case R.id.next_page_bt:
-                mCurrentPage++;
-                refreshPersons();
-            default:
-                Log.w(LOG_TAG, "unhandled click event");
+    public void onClickPrevious (View v) {
+        if (mCurrentPage > 1) {
+            mCurrentPage--;
+            refreshPopularPersons();
+        } else {
+            Toast toast = Toast.makeText(this, R.string.toast_no_previous_page, Toast.LENGTH_SHORT);
+            toast.show();
         }
+    }
+
+    public void onClickNext(View view) {
+        mCurrentPage++;
+        refreshPopularPersons();
     }
 }
--- app/src/main/java/com/lpastyle/clswtmdbapi2022/PersonListAdapter.java
+++ app/src/main/java/com/lpastyle/clswtmdbapi2022/PersonListAdapter.java
@@ -1,21 +1,19 @@
 package com.lpastyle.clswtmdbapi2022;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.lpastyle.clswtmdbapi2022.databinding.PersonItemBinding;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class PersonListAdapter extends RecyclerView.Adapter<PersonListAdapter.ViewHolder> {
+public class PersonListAdapter extends RecyclerView.Adapter<PersonListAdapter.PersonViewHolder> {
 
-    private List<PersonData> mPersons;
+    private final List<PersonData> mPersons;
 
     public PersonListAdapter(List<PersonData> persons) {
         this.mPersons = persons;
@@ -23,14 +21,54 @@ public class PersonListAdapter extends RecyclerView.Adapter<PersonListAdapter.Vi
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // Create a new view
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.person_item, parent, false);
-        return new ViewHolder(v);
+    public PersonListAdapter.PersonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        PersonItemBinding binding = PersonItemBinding.inflate(layoutInflater, parent, false);
+        return new PersonViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull PersonListAdapter.PersonViewHolder holder, int position) {
+        PersonData curItem = mPersons.get(position);
+
+        holder.binding.nameTv.setText(curItem.getName());
+        holder.binding.popularityTv.setText(String.valueOf(curItem.getPopularity()));
+
+        Picasso.get()
+                .load(ApiClient.IMAGE_BASE_URL + curItem.getProfilePath())
+                .placeholder(android.R.drawable.sym_def_app_icon)
+                .error(android.R.drawable.ic_menu_help)
+                .into(holder.binding.photoIv);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPersons.size();
+    }
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+        PersonItemBinding binding;
+
+        public PersonViewHolder(@NonNull PersonItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    /* ---------
+     * REMINDER: The "old" way to proceed using hand made view holdler
+     * ---------
+
+    @NonNull
+    @Override
+    public PersonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.person_item, parent, false);
+        return new PersonViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PersonViewHolder holder, int position) {
         {
             PersonData curItem = mPersons.get(position);
             holder.nameTv.setText(curItem.getName());
@@ -44,26 +82,24 @@ public class PersonListAdapter extends RecyclerView.Adapter<PersonListAdapter.Vi
         }
     }
 
-
     @Override
     public int getItemCount() {
         return mPersons.size();
     }
 
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class PersonViewHolder extends RecyclerView.ViewHolder {
         ImageView photoIv;
         TextView nameTv;
         TextView popularityTv;
 
-        public ViewHolder(View v) {
+        public PersonViewHolder(View v) {
             super(v);
             photoIv = v.findViewById(R.id.photo_iv);
             nameTv = v.findViewById(R.id.name_tv);
             popularityTv = v.findViewById(R.id.popularity_tv);
         }
     }
-
+    */
 
 }
 
